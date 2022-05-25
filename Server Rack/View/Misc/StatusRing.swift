189c8e9b8f2e6dbd237df343a210c58cd51b5907
JonathanDowdell <jdowde2@wgu.edu>
2022-05-25 13:21:18 -0500

    Ring

---
+++ Server Rack/View/Misc/StatusRing.swift
@@ -0,0 +1,44 @@
+//
+//  StatusRing.swift
+//  Server Rack
+//
+//  Created by Jonathan Dowdell on 5/24/22.
+//
+
+import SwiftUI
+
+struct StatusRing: View {
+    
+    var percent: CGFloat
+    var startAngle: CGFloat
+    var ringWidth: CGFloat
+    var ringColor: Color
+    var backgroundColor: Color
+    var drawnClockwise: Bool
+    
+    var body: some View {
+        ZStack {
+            RingShape()
+                .stroke(style: StrokeStyle(lineWidth: ringWidth, lineCap: .round))
+                .fill(backgroundColor)
+            
+            RingShape(percent: percent, startAngle: startAngle, drawnClockwise: drawnClockwise)
+                .stroke(style: StrokeStyle(lineWidth: ringWidth, lineCap: .round))
+                .fill(ringColor)
+        }
+    }
+}
+
+struct CircleItem_Previews: PreviewProvider {
+    static var previews: some View {
+        StatusRing(
+            percent: 50.0,
+            startAngle: 90.0,
+            ringWidth: 5,
+            ringColor: .green,
+            backgroundColor: Color(.systemGray2),
+            drawnClockwise: true
+        )
+            .frame(width: 100, height: 100, alignment: .center)
+    }
+}
